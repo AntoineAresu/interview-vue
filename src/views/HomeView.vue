7a73a995355bd2aaa428e790a8a435bda9ae0bb7
AntoineAresu <you@example.com>
2022-05-05 11:19:25 +0200

    :arrow_up: [Vuetify] Install

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,80 +1,18 @@
-<script setup lang="ts">
-import axios from 'axios';
-import { ref, onMounted } from 'vue'
-import names from "../names";
-
-const avatarEndpoint = "https://avatars.dicebear.com/v2/avataaars/";
-const apiOptions = "options[mood][]=happy";
-
-const backendUrl = "http://localhost:8000";
-const beneficiariesEndpoint = `${backendUrl}/api/beneficiaries`;
-const loginEndpoint = `${backendUrl}/login`;
-
-function getAvatar(name) {
-  return`${avatarEndpoint}${name}.svg?${apiOptions}`;
-}
-
-const beneficiaries = ref(null)
-  
-onMounted(async() => {
-  const response = await axios.get(beneficiariesEndpoint);
-  beneficiaries.value = response.data;
-  console.log(beneficiaries);
-})
-
-const beneficiaryNames = [...Array(10).keys()].map(number => ({
-  name: names[Math.floor(Math.random() * names.length)]
-}));
-
-</script>
-
-}
 <template>
-  <div class="App">
-    <header class="App-header">
-      <h1>Gestionnaire de bénéficaires Reconnect</h1>
-      <hr/>
-      <h3>Non stockés</h3>
-      <div class="Beneficiaries-list">
-          <div v-for="beneficiary in beneficiaryNames" :key="beneficiary.id"  class="Beneficiary-card">
-              <img :src="getAvatar(beneficiary.name)"/>
-              <span>{{ beneficiary.name }}</span>
-          </div>
-      </div>
-      <hr/>
-      <h3>En base de donnés</h3>
-      <div class="Beneficiaries-list">
-        <div v-for="beneficiary in beneficiaries" :key="beneficiary.id" class="Beneficiary-card">
-            <img :src="getAvatar(beneficiary.name)"/>
-            <span>{{ beneficiary.name }}</span>
-        </div>
-      </div>
-    </header>
-  </div>
+  <hello-world />
 </template>
 
-<style>
-.Beneficiaries-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-.Beneficiary-card {
-  width: 200px;
-}
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
 
-.App-header {
-  padding-left: 200px;
-  padding-right: 200px;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-.App {
-  text-align: center;
-}
-</style>
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
